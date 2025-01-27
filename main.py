a14d01bd525e0670aac0ad9eedfc194cfa5082db
claymrx571 <claymrx571@gmail.com>
2025-01-27 23:41:36 +0500

    infobot

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from asyncio import run
+from func import get_user_info
 
 dp = Dispatcher()
 
-async def echo(message: types.Message, bot: Bot):
-    await message.copy_to(chat_id=message.chat.id)
+async def startup_answer(bot: Bot):
+    await bot.send_message(chat_id=7192104018, text="Bot ishga tushirildi! ✅")
+
+async def shutdown_answer(bot: Bot):
+    await bot.send_message(chat_id=7192104018, text="Bot ishdan to'xtadi! ❌")
 
 async def start():
-    dp.message.register(echo)
+    dp.startup.register(startup_answer)
+    dp.message.register(get_user_info)
+    dp.shutdown.register(shutdown_answer)
     bot = Bot("7920759596:AAGNAmgDFeaY6eQyCWZNMz0M29Dqtf25-ik")
     await dp.start_polling(bot, polling_timeout=1)
 
